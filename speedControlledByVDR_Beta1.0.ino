55a07ab46eda2038c34bfd4dc23f9ff59ddc17d2
Liu-Lingfei <56315716@qq.com>
2017-08-20 16:32:41 +0800

    Update speedControlledByVDR_Beta1.0.ino

--- speedControlledByVDR_Beta1.0.ino
+++ speedControlledByVDR_Beta1.0.ino
@@ -4,21 +4,18 @@ int analogOutPin = 9;
 int sensorValue0 = 0;
 int sensorValue1 = 0;
 int Speed = 0;
-
 int outputValue = 0;
-
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-
   sensorValue0 = analogRead( analogInPin0);
   sensorValue1 = analogRead( analogInPin1);
-
   Speed = sensorValue0 - sensorValue1;
   outputValue = map( Speed,-1023,1023,140,10);
+  
   analogWrite( analogOutPin, outputValue);
 
   Serial.print("\speed: ");
